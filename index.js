a43c3ef1d24a71660e549fda41049e5e1c9fb5be
张雨菲 <zhangyufei@zhangyufeideair.coldspring.marian.edu>
2018-05-16 18:12:22 -0400

    Create functions to return the inputs

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 const form = document.querySelector('form')
 
-const collectCountry = []
-const collectCity = []
-const collectContinent = []
+function handleSubmit(ev){
+    ev.preventDefault();
+    const collectCountry = []
+    const collectContinent = []
+    const a = ev.target
+    const country = a.country.value
+    collectCountry.push(country)
+    const continent = a.continent.value
+    collectContinent.push(continent)
+    const collects = {
+        'country': country,
+        'continent': continent,
+    }
+    const collections = document.querySelector('#collections')
+    collections.appendChild(renderList(collects))
+
+    a.reset()
+    a.country.focus()
+}
+function renderList(data){
+    const list = document.createElement('ul')
+    const keys = Object.keys(data)
+    const item = document.createElement('li')
+    keys.forEach(label => {
+        item.textContent += data[label]+' '
+    })
+    list.appendChild(item)
+    return list
+}
+
+
+
+form.addEventListener('submit',handleSubmit)
+
